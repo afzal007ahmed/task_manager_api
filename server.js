439ad6f0e0e64703121835570431064ad6f1597e
Afzal <afzalahmeddc@gmail.com>
2026-01-28 16:03:19 +0530

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { app } = require("./app");
 const { config } = require("./config/index.js");
+const { logger } = require("./config/winston.config.js");
 const { sequelize } = require("./connection/sequelize.js");
 require("./models/index.js");
 
@@ -8,7 +9,7 @@ async function Server() {
   await sequelize.sync();
 
   app.listen(config.port, () => {
-    console.log("Server listening at port : ", config.port);
+    logger.info(`Server is listening on : ${config.port}`) ;
   });
 }
 
